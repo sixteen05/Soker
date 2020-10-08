a029c8f91dfc615c1767a17bc77e3f3f46b306ca
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-10-08 21:52:00 +0530

    Got basic Gradle application running
    Signed-off-by: Lalit Umbarkar <lalit.umbarkar@customerxps.com>

---
+++ jars/card-game/src/main/java/com/redit/core/Main.java
@@ -0,0 +1,15 @@
+package com.redit.core;
+
+/**
+ * Created By : Lalit Umbarkar
+ * Created On : 9/10/2020
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Works");
+    }
+}
+
+
+
---
+++ jars/card-game/src/main/java/com/redit/models/Card.java
@@ -0,0 +1,11 @@
+package com.redit.models;
+
+/**
+ * Created By : Lalit Umbarkar
+ * Created On : 9/10/2020
+ */
+
+public class Card {
+
+}
+
